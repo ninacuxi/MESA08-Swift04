0c8373d3cd5108be42f604702c2e345188fbbbeb
nina <ninacuxi@gmail.com>
2017-09-06 15:41:48 +0800

    Initial Commit

---
+++ MySwift04/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MySwift04
+//
+//  Created by Nina on 2017/9/6.
+//  Copyright © 2017年 Nina Big Company. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
